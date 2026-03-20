ab8e7d4ae315ef54122db3e6eb3cf3142fa0df59
Kristian Rucek <xrucekk00@vutbr.fit.cz>
2026-03-20 17:26:39 +0100

    Refactored Tcp scan, still filtering errors

--- src/tcp_scan.c
+++ src/tcp_scan.c
@@ -35,8 +35,6 @@ int get_local_ip_address(const char *desired_interface_name, struct in_addr *res
         perror("getifaddrs");
         return ERROR;
     }
-
-    int found = 0;
     
     if (desired_interface_name != NULL) {
         for (current_interface = interface_list_head; current_interface != NULL; current_interface = current_interface->ifa_next) {
@@ -46,15 +44,11 @@ int get_local_ip_address(const char *desired_interface_name, struct in_addr *res
 
             if (strcmp(current_interface->ifa_name, desired_interface_name) == 0) {
                 *result_ip = ((struct sockaddr_in *)current_interface->ifa_addr)->sin_addr;
-                found = 1;
-                break;
+                freeifaddrs(interface_list_head);
+                return OK;
             }
         }
     }
-
-    freeifaddrs(interface_list_head);
-    
-    if (found) return OK;
     return ERROR;
 }
 
@@ -110,23 +104,74 @@ int resolve_tcp_targets(const Config *config, struct addrinfo **targets) {
     return OK;
 }
 
-int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target) {
-    struct in_addr local_ip;
-    if (get_local_ip_address(config->interface_name, &local_ip) != OK) {
-        fprintf(stderr, "Could not determine local IP address. Checksum calculation would fail.\n");
-        return ERROR;
-    }
+int catch_tcp_response(pcap_t *pcap_handle, uint16_t expected_source_port, uint16_t expected_dest_port) {
+    struct pcap_pkthdr *packet_header;
+    const u_char *packet_data;
 
-    // Open handle for libpcap
+    while (1) {
+        int result = pcap_next_ex(pcap_handle, &packet_header, &packet_data);
+        if (result == 0) {
+            return PORT_STATUS_FILTERED; // Timeout
+        }
+        if (result < 0) {
+            return ERROR; // Error
+        }
+
+        int link_header_length = 14; // Default for Ethernet
+        int link_type = pcap_datalink(pcap_handle);
+        if (link_type == DLT_LINUX_SLL) link_header_length = 16;
+        else if (link_type == DLT_NULL) link_header_length = 4;
+
+        struct iphdr *ip_header = (struct iphdr *)(packet_data + link_header_length);
+        struct tcphdr *tcp_header = (struct tcphdr *)((u_char *)ip_header + (ip_header->ihl * 4));
+
+        if (ntohs(tcp_header->dest) == expected_source_port && ntohs(tcp_header->source) == expected_dest_port) {
+            return tcp_header->syn && tcp_header->ack ? PORT_STATUS_OPEN : tcp_header->rst ? PORT_STATUS_CLOSED : PORT_STATUS_FILTERED;
+        }
+    }
+}
+
+int create_tcp_syn_packet(struct in_addr source_ip, struct in_addr dest_ip, uint16_t source_port,
+     Ip_header *ip_header, Tcp_header *tcp_header) {
+
+    memset(ip_header, 0, sizeof(Ip_header));
+    memset(tcp_header, 0, sizeof(Tcp_header));
+
+    ip_header->version_ihl = (4 << 4) | (sizeof(Ip_header) / 4);
+    ip_header->dscp_ecn = 0;
+    ip_header->total_length = htons(sizeof(Ip_header) + sizeof(Tcp_header));
+    ip_header->identification = htons(54321);
+    ip_header->flags_fragment_offset = htons(0);
+    ip_header->ttl = 64;
+    ip_header->protocol = IPPROTO_TCP;
+    ip_header->source_ip = source_ip;
+    ip_header->dest_ip = dest_ip;
+    ip_header->header_checksum = checksum(ip_header, sizeof(Ip_header));
+
+    tcp_header->source_port = htons(source_port);
+    // dest_port set by caller per port
+    tcp_header->seq_num = htonl(123456789);
+    tcp_header->ack_num = 0;
+    tcp_header->data_offset = (sizeof(Tcp_header) / 4) << 4;
+    tcp_header->flags = 0x02; // SYN
+    tcp_header->window_size = htons(65535);
+    tcp_header->urgent_pointer = 0;
+    
+    // Checksum will be calculated by caller after setting dest_port
+
+    return OK;
+}
+
+
+pcap_t *initialize_pcap_listener(const Config *config, struct addrinfo *target) {
     char error_buffer[PCAP_ERRBUF_SIZE];
     pcap_t *pcap_handle;
     const char *interface_name = config->interface_name ? config->interface_name : "any";
 
-    // Use pcap_create and pcap_activate for better control over timeout
     pcap_handle = pcap_create(interface_name, error_buffer);
     if (pcap_handle == NULL) {
         fprintf(stderr, "Couldn't create pcap handle for device %s: %s\n", interface_name, error_buffer);
-        return ERROR;
+        return NULL;
     }
 
     if (pcap_set_snaplen(pcap_handle, 65535) != 0) {
@@ -143,7 +188,7 @@ int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target)
     if (pcap_activate(pcap_handle) != 0) {
         fprintf(stderr, "Couldn't activate pcap handle: %s\n", pcap_geterr(pcap_handle));
         pcap_close(pcap_handle);
-        return ERROR;
+        return NULL;
     }
 
     struct bpf_program filter_program;
@@ -155,12 +200,28 @@ int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target)
     if (pcap_compile(pcap_handle, &filter_program, filter_expression, 0, PCAP_NETMASK_UNKNOWN) == -1) {
          fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_expression, pcap_geterr(pcap_handle));
          pcap_close(pcap_handle);
-         return ERROR;
+         return NULL;
     }
     if (pcap_setfilter(pcap_handle, &filter_program) == -1) {
          fprintf(stderr, "Couldn't install filter %s: %s\n", filter_expression, pcap_geterr(pcap_handle));
          pcap_close(pcap_handle);
-         return ERROR;
+         return NULL;
+    }
+
+    return pcap_handle;
+}
+
+int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target) {
+    struct in_addr local_ip;
+    if (get_local_ip_address(config->interface_name, &local_ip) != OK) {
+        fprintf(stderr, "Could not determine local IP address. Checksum calculation would fail.\n");
+        return ERROR;
+    }
+
+    // Open handle for libpcap
+    pcap_t *pcap_handle = initialize_pcap_listener(config, target);
+    if (pcap_handle == NULL) {
+        return ERROR;
     }
 
     // Open RAW socket for sending
@@ -170,6 +231,9 @@ int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target)
         pcap_close(pcap_handle);
         return ERROR;
     }
+
+    char target_ip_string[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &((struct sockaddr_in *)target->ai_addr)->sin_addr, target_ip_string, sizeof(target_ip_string));
 
     // Set IP_HDRINCL to tell the kernel we provide our own IP header
     int on = 1;
@@ -187,41 +251,14 @@ int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target)
         return ERROR;
     }
 
-    // Prepare headers for sending
     Ip_header ip_header;
     Tcp_header tcp_header;
-    memset(&ip_header, 0, sizeof(ip_header));
-    memset(&tcp_header, 0, sizeof(tcp_header));
-
-    ip_header.version_ihl = (4 << 4) | (sizeof(Ip_header) / 4);
-    ip_header.dscp_ecn = 0;
-    ip_header.total_length = htons(sizeof(Ip_header) + sizeof(Tcp_header));
-    ip_header.identification = htons(54321);
-    ip_header.flags_fragment_offset = htons(0);
-    ip_header.ttl = 64;
-    ip_header.protocol = IPPROTO_TCP;
-
-    ip_header.source_ip = local_ip; 
-    
-    ip_header.dest_ip = ((struct sockaddr_in *)target->ai_addr)->sin_addr;
-    ip_header.header_checksum =0; 
-    ip_header.header_checksum = checksum(&ip_header, sizeof(Ip_header));
-
-    tcp_header.source_port = htons(MY_RANDOM_PORT);
-    tcp_header.seq_num = htonl(SEQ_NUM);
-    tcp_header.ack_num = 0;
-    tcp_header.data_offset = (sizeof(Tcp_header) / 4) << 4;
-    tcp_header.flags = 0x02; // SYN
-    tcp_header.window_size = htons(65535);
-    tcp_header.urgent_pointer = 0;
-   
     struct sockaddr_in *destination_socket_address = (struct sockaddr_in *)target->ai_addr;
-    
-    struct pcap_pkthdr *pcap_packet_header;
-    const u_char *captured_packet;
 
+    create_tcp_syn_packet(local_ip, destination_socket_address->sin_addr, MY_RANDOM_PORT, &ip_header, &tcp_header);
+   
     for (int port_number=1; port_number<MAX_PORTS; port_number++) {
-         if (config->tcp_ports[port_number]) {
+        if (config->tcp_ports[port_number]) {
              tcp_header.dest_port = htons(port_number);
              tcp_header.checksum = 0; // Reset checksum
 
@@ -229,39 +266,18 @@ int scan_tcp_ports_for_one_target(const Config *config, struct addrinfo *target)
              
              send_tcp_syn(raw_socket, destination_socket_address, &ip_header, &tcp_header);
 
-             while(1) {
-                 int pcap_result = pcap_next_ex(pcap_handle, &pcap_packet_header, &captured_packet);
-                 if (pcap_result == 0) {
-                     // Timeout expired
-                     printf("%d/tcp filtered\n", port_number);
-                     break; 
-                 }
-                 if (pcap_result < 0) break; // Error
+             int response_status = catch_tcp_response(pcap_handle, MY_RANDOM_PORT, port_number);
 
-                 // Handle different link types, On Ethernet, the first 14 bytes are MAC addresses
-                 int link_header_length = 14;
-                 int link_type = pcap_datalink(pcap_handle);
-                 if (link_type == DLT_LINUX_SLL) link_header_length = 16;
-                 else if (link_type == DLT_NULL) link_header_length = 4;
-                 
-                 struct iphdr *ip_header_ptr = (struct iphdr*)(captured_packet + link_header_length);
-                 struct tcphdr *tcp_header_ptr = (struct tcphdr*)((u_char*)ip_header_ptr + (ip_header_ptr->ihl * 4));
-
-                 // Validate packet is for this probe
-                 if (ntohs(tcp_header_ptr->dest) == MY_RANDOM_PORT && ntohs(tcp_header_ptr->source) == port_number) {
-                     if (tcp_header_ptr->syn && tcp_header_ptr->ack) {
-                         printf("%d/tcp open\n", port_number);
-                     } else if (tcp_header_ptr->rst) {
-                         printf("%d/tcp closed\n", port_number);
-                     } else {
-                         printf("%d/tcp filtered\n", port_number);
-                     }
-                     break; // Done with this port
-                 }
-                 // If not matching, continue loop (catch next packet)
+             if (response_status == PORT_STATUS_OPEN) {
+                    printf("%s %d tcp open\n", target_ip_string, port_number);
+             } else if (response_status == PORT_STATUS_CLOSED) {
+                    printf("%s %d tcp closed\n", target_ip_string, port_number);
+             } else if (response_status == PORT_STATUS_FILTERED) {
+                    printf("%s %d tcp filtered\n", target_ip_string, port_number); 
              }
-         }
-    }
+        }
+    }    
+                
     close(raw_socket);
     pcap_close(pcap_handle);
     return OK;
